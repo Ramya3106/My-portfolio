becab9c1621945d01c4963b1464e0ead80318161
Ramya3106 <divyaramya1422@gmail.com>
2024-09-18 07:54:11 -0700

    Initialized portfolio project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,43 @@
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
 
-const config: Config = {
+const config = {
+  darkMode: ["class"],
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+	],
+  prefix: "",
   theme: {
+    container: {
+      center: true,
+      padding: "15px"
+    },
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '960px',
+        xl: '1200px',
+
+      },
+      fontfamily: {
+        primary: "var(--font-jetBrainsMono)",
+      },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: "#1c1c22",
+        accent: {
+          DEFAULT: "#00ff99",
+          hover: "#00e187",
+        },
+      },
+      keyframes: {
+      },
+      animation: {
+      },
       },
     },
-  },
-  plugins: [],
-};
-export default config;
+  plugins: [require("tailwindcss-animate")],
+} ;
+
